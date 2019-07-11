d501e7947617795d1a2e875e0c9d9fa0d64cf282
Ryan Nowak <nowakra@gmail.com>
2019-07-11 11:29:26 -0700

    Fail faster in Blazor E2E tests
    This change adds a fail-fast mechanism to our E2E tests based on
    the browser console. This will fail super hard if an unhandled exception
    is thrown.
    
    I think it would be interesting to also see if we could do the same
    thing for 404s.
    
    The goal of this change is to make it so that the E2E tests can fail
    faster (3-4s) than the 30s timeout in the case that something
    catastrophic happens. As a nice side benefit you get to see the
    exception message.

--- src/Shared/E2ETesting/BrowserTestBase.cs
+++ src/Shared/E2ETesting/BrowserTestBase.cs
@@ -1,11 +1,16 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using Xunit;
 using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace Microsoft.AspNetCore.E2ETesting
 {
@@ -50,6 +55,40 @@ namespace Microsoft.AspNetCore.E2ETesting
 
         protected virtual void InitializeAsyncCore()
         {
+            // Clear logs - we check these during tests in some cases.
+            // Make sure each test starts clean.
+            ((IJavaScriptExecutor)Browser).ExecuteScript("console.clear()");
+        }
+
+        protected IWebElement WaitUntilExists(By findBy, int timeoutSeconds = 10, bool throwOnError = false)
+        {
+            List<LogEntry> errors = null;
+            IWebElement result = null;
+            new WebDriverWait(Browser, TimeSpan.FromSeconds(timeoutSeconds)).Until(driver =>
+            {
+                if (throwOnError && Browser.Manage().Logs.AvailableLogTypes.Contains(LogType.Browser))
+                {
+                    // Fail-fast if any errors were logged to the console.
+                    var log = Browser.Manage().Logs.GetLog(LogType.Browser);
+                    if (log.Any(entry => entry.Level == LogLevel.Severe))
+                    {
+                        errors = log.Where(entry => entry.Level == LogLevel.Severe).ToList();
+                        return true;
+                    }
+                }
+
+                return (result = driver.FindElement(findBy)) != null;
+            });
+
+            if (errors != null)
+            {
+                var message =
+                    $"Encountered errors while looking for '{findBy}'." + Environment.NewLine +
+                    string.Join(Environment.NewLine, errors);
+                throw new XunitException(message);
+            }
+
+            return result;
         }
     }
 }
